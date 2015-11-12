524b4700139e361fa5d3d5555b08b8844b4576e4
Andres Hazard <andreshazard@localhost.localdomain>
2015-11-12 17:47:47 -0300

    moddified script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources tmux.conf bash_aliases"    # list of files/folders to symlink in homedir
+files="bashrc vimrc tmux.conf bash_aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
